e7abadb06e21c66c3a0298b4db53396851355a27
Martin Klepsch <martinklepsch@googlemail.com>
2016-04-20 18:58:18 +0200

    clean up sqs namespace + use timbre

--- src/oc/bot/sqs.clj
+++ src/oc/bot/sqs.clj
@@ -1,6 +1,7 @@
 (ns oc.bot.sqs
   (:require [amazonica.aws.sqs :as sqs]
             [com.stuartsierra.component :as component]
+            [taoensso.timbre :as timbre]
             [manifold.stream :as s]
             [manifold.time :as t]
             [manifold.deferred :as d]
@@ -25,52 +26,48 @@
   ;; (prn 'deleting-msg (:body msg))
   (sqs/delete-message creds (assoc msg :queue-url queue-url)))
 
-;; (defn relay-message-to-slack
-;;   "Relay a given message to slack, mostly used for testing purposes"
-;;   [slack-conn msg]
-;;   (slack/send-message! (-> sys :slack) (read-string (:body msg)))
-;;   msg)
-
 (defn sqs-process*
   "Yield a deferred that will ultimately delete the message put into it
   from SQS unless it fails while handling the message. Logs if it does."
   [msg-handler msg-delete]
   (let [res (d/deferred)]
     (-> res
-        (d/chain msg-handler msg-delete (fn delete-report [x] prn 'msg-delete-result x))
+        (d/chain msg-handler msg-delete (fn delete-report [x] (prn 'msg-delete-result x)))
         (d/catch Exception #(println "whoops, that didn't work:" %)))
     res))
 
 (defn dispatch-message
   "Check for a message and, if one is available, put it into the given deferrred"
   [queue-url deferred]
+  (timbre/debugf "Checking for message in queue: %s\n" queue-url)
   (when-let [m (get-message queue-url)]
+    (timbre/debugf "Got message from queue: %s\n" queue-url)
     (d/success! deferred m)))
 
 (defrecord SQSListener [queue-url message-handler]
   ;; Implement the Lifecycle protocol
   component/Lifecycle
   (start [component]
-    (println ";; Starting SQSListener")
+    (timbre/info "Starting SQSListener")
     (let [delete!   (partial delete-message! creds queue-url)
           handle!   (partial message-handler component)   
           processor (fn [] (sqs-process* handle! delete!))
           retriever (t/every 3000 #(dispatch-message queue-url (processor)))]
-      ;; (s/consume (partial (:message-handler component) conn (swap! msg-idx inc)) conn)
+      ;; (s/consume (partial (:message-nfhandler component) conn (swap! msg-idx inc)) conn)
       ;; (d/catch conn #(str "something unexpected: " (.getMessage %))) ; not sure if this actually does anything 
       (assoc component :retriever retriever)))
 
   (stop [component]
-    (println ";; Stopping SQSListener")
+    (timbre/info "Stopping SQSListener")
     (when-let [r (:retriever component)] (r))
     (dissoc component :retriever)))
 
 (defn sqs-listener [queue-url message-handler]
   (map->SQSListener {:queue-url queue-url
-                     :message-handler (or message-handler #(prn 'sqs-default-handler %))}))
+                     :message-handler (or message-handler #(do (prn 'sqs-default-handler %1 %2) %2))}))
 
 (comment
-  (def sqs (map->SQSListener {:queue-url "https://sqs.us-east-1.amazonaws.com/892554801312/my-queue"}))
+  (def sqs (sqs-listener "https://sqs.us-east-1.amazonaws.com/892554801312/my-queue" nil))
   
   (alter-var-root #'sqs component/start)
 
@@ -87,6 +84,14 @@
   ;; full list of attributes at
   ;; http://docs.aws.amazon.com/AWSJavaSDK/latest/javadoc/com/amazonaws/services/sqs/model/GetQueueAttributesRequest.html
 
+  (def st (s/periodically 3000 #(get-message (e/env :aws-sqs-queue))))
+
+  (s/periodically 3000 #(prn :x))
+
+  (get-message (e/env :aws-sqs-queue))
+
+  (e/env :aws-sqs-queue)
+
   (sqs/create-queue creds "DLQ")
 
   (sqs/list-queues creds)
@@ -101,7 +106,7 @@
 
   (def msgs (sqs/receive-message creds queue))
 
-  (time (sqs/receive-message creds queue))
+  (sqs/receive-message creds (e/env :aws-sqs-queue))
 
 
   ;; (sqs/receive-message creds queue)
