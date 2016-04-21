0c795bcf31c6280c28dbbd957e5766dc2ec6e933
Martin Klepsch <martinklepsch@googlemail.com>
2016-04-21 18:22:00 +0200

    add more script triggers to main ns

--- src/oc/bot.clj
+++ src/oc/bot.clj
@@ -3,9 +3,12 @@
             [amazonica.aws.sqs :as aws-sqs]
             [clojure.java.io :as io]
             [environ.core :as e]
+            [manifold.stream :as s]
+            [taoensso.timbre :as timbre]
+            [oc.bot.sqs :as sqs]
             [oc.bot.slack :as slack]
-            [oc.bot.message :as msg]
-            [oc.bot.sqs :as sqs]))
+            [oc.bot.conversation :as conv]
+            [oc.bot.message :as msg]))
 
 (defn system [config-options]
   (let [{:keys [sqs-queue sqs-msg-handler]} config-options]
@@ -20,39 +23,50 @@
   (let [msg-body   (read-string (:body msg))
         bot-token  (-> msg-body :bot :token)
         slack-conn (or (slack/connection-for (:slack sys) bot-token)
-                       (slack/initialize-connection! (:slack sys) bot-token slack-handler))]
+                       (slack/initialize-connection! (:slack sys) bot-token))]
     ;; (prn 'slack-conn slack-conn)
-    (doseq [m (msg/messages-for :onboard (-> msg-body :script :params))]
-      (slack/send-event! slack-conn {:type "typing"
-                                     :channel (-> msg-body :receiver :id)})
-      (slack/send-event! slack-conn {:type "message"
-                                     :text m
-                                     :channel (-> msg-body :receiver :id)})))
+    (timbre/infof "Received message from SQS: %s\n" msg-body)
+    (s/put! (:in-proxy slack-conn) (assoc msg-body :type ::initialize)))
     msg)
 
 (comment
-  (def bot-user-id "U10AR0H50")
-  (def bot-testing-ch "C10A1P4H2")
+  (do 
+    (def bot-user-id "U10AR0H50")
+    (def bot-testing-ch "C10A1P4H2")
+    (def dm-testing-ch "D108XUAFM")
 
-  (def test-msg {:type "message" :channel bot-testing-ch :text "Slack connection pooling works!"
-                 :bot-token (e/env :slack-bot-token) :bot-user-id bot-user-id})
+    (def test-msg {:type "message" :channel bot-testing-ch :text "Slack connection pooling works!"
+                   :bot-token (e/env :slack-bot-token) :bot-user-id bot-user-id})
 
-  (def test-onboard-trigger
-    {:script {:id :onboard :params {:name "Sarah" :company "Flickr" :company-dashboard "https://opencompany.com/flickr" :contact-person "Tom"}}
-     :receiver {:type :channel :id bot-testing-ch}
-     :bot {:token (e/env :slack-bot-token) :id bot-user-id}})
+    (defn test-onboard-trigger []
+      {:diff     (rand-int 1000)
+       :script   {:id :onboard :params {:name "Sarah" :company-name "Flickr" :company-slug "flickr" :company-dashboard "https://opencompany.com/flickr" :company-description "The home for all your photos." :contact-person "Tom" :currency "$"}}
+       :receiver {:type :channel :id dm-testing-ch}
+       :bot      {:token (e/env :slack-bot-token) :id bot-user-id}})
+    (defn test-su-trigger []
+      {:diff     (rand-int 1000)
+       :script   {:id :stakeholder-update :params {:name "Sarah" :company-name "Flickr" :stakeholder-update-link "https://opencompany.com/flickr"}}
+       :receiver {:type :channel :id dm-testing-ch}
+       :bot      {:token (e/env :slack-bot-token) :id bot-user-id}})
+    (defn test-onboard-user-trigger []
+      {:diff     (rand-int 1000)
+       :script   {:id :onboard-user :params {:name "Sarah" :company-name "Flickr" :company-dashboard "https://opencompany.com/flickr" :contact-person "@stuart"}}
+       :receiver {:type :channel :id dm-testing-ch}
+       :bot      {:token (e/env :slack-bot-token) :id bot-user-id}})
+    (defn test-onboard-user-authenticated-trigger []
+      {:diff     (rand-int 1000)
+       :script   {:id :onboard-user :params {:name "Sarah" :company-name "Flickr" :company-dashboard "https://opencompany.com/flickr"}}
+       :receiver {:type :channel :id dm-testing-ch}
+       :bot      {:token (e/env :slack-bot-token) :id bot-user-id}})
+    
+    )
 
-  (aws-sqs/send-message sqs/creds (e/env :aws-sqs-queue) test-onboard-trigger)
+  (aws-sqs/send-message sqs/creds (e/env :aws-sqs-queue) (test-onboard-user-trigger))
 
   (def sys (system {:sqs-queue (e/env :aws-sqs-queue)
-                    :sqs-msg-handler sqs-handler
-                    ;; :slack-msg-handler (fn [conn msg-idx msg] (prn msg))
-                    ;; :slack-token (e/env :slack-bot-token)
-                    }))
+                    :sqs-msg-handler sqs-handler}))
 
   (alter-var-root #'sys component/start)
-
-  (-> sys :slack :connections deref keys)
 
   (alter-var-root #'sys component/stop)
 
@@ -63,7 +77,7 @@
   ;; (let [dd (d/deferred)]
   ;;   (-> dd
   ;;       (d/chain #(do (assert (string? %) "invalid") %) prn)
-  ;;       (d/catch #(str "something unexpected:" (.getMessage %))))
+  ;;       (d/catch #(println "something unexpected:" (.getMessage %))))
   ;;   (d/success! dd nil))
 
   ;; (try
