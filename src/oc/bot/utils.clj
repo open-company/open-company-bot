bdcc442f817410246c9ec1cd4378dbf49e4fe976
Martin Klepsch <martinklepsch@googlemail.com>
2016-04-21 18:13:48 +0200

    small utils ns

---
+++ src/oc/bot/utils.clj
@@ -0,0 +1,12 @@
+(ns oc.bot.utils)
+
+(defn predicate-map-lookup [pred-map v]
+  (reduce (fn [acc pred]
+            (if (pred v)
+              (conj acc (get pred-map pred))
+              acc))
+          (list)
+          (keys pred-map)))
+
+(defn next-in [xs x]
+  (second (drop-while #(not= % x) xs)))
