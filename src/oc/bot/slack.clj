33e131ad31403d2ee5f0d96a8570ef938ec79592
Martin Klepsch <martinklepsch@googlemail.com>
2016-04-21 18:20:21 +0200

    automatically parse/stringify/ident slack messages

--- src/oc/bot/slack.clj
+++ src/oc/bot/slack.clj
@@ -1,7 +1,9 @@
 (ns oc.bot.slack
-  (:require [aleph.http :as http]
+  (:require [oc.bot.conversation :as conv]
+            [aleph.http :as http]
             [com.stuartsierra.component :as component]
             [clojure.string :as string]
+            [taoensso.timbre :as timbre]
             [manifold.stream :as s]
             [manifold.time :as t]
             [manifold.deferred :as d]
@@ -30,7 +32,8 @@
   "Retrieve a websocket connection URL"
   [bot-token]
   (let [response (-> @(http/get rtm-socket-url {:query-params {:token bot-token :no_unreads true} :as :json}) :body)]
-    (when (:ok response) (:url response))))
+    (when (:ok response)
+      (:url response))))
 
 (defn send-ping!
   "Send a ping message to the Slack RTM API to make sure the connection stays alive
@@ -39,43 +42,63 @@
   ;; Add some error handling/logging
   (s/put! conn (chesire/generate-string {:type "ping" :id id})))
 
+(defn add-id-and-jsonify [out id msg]
+  (timbre/info "Sending to Slack:" msg)
+  (->> (assoc msg :id id)
+       (chesire/generate-string)
+       (s/put! out)))
+
+(defn parse [msg out]
+  (let [m (chesire/parse-string msg keyword)]
+    (timbre/info m)
+    (when-not (get m :ok ::ok)
+      (timbre/warn "Error event from Slack" m))
+    (s/put! out m)))
+
 (defrecord SlackConnection [ws-url]
   component/Lifecycle
   (start [component]
-    (println ";; Starting SlackConnection" ws-url)
-    ;; https://github.com/ztellman/manifold/issues/73
-    ;; (assert ws-url "Websocket URL required to establish connection")
+    (timbre/infof "Starting SlackConnection %s\n" ws-url)
+    (assert ws-url "Websocket URL required to establish connection")
     (let [msg-idx    (atom 0) ; messages need pos-int ids for same conn
           conn       @(aleph.http/websocket-client ws-url)
+          out-proxy  (s/stream)
+          in-proxy   (s/stream)
+          ;; TODO the keep alive routine should also check if the connection is open and create a new connection if necessary
           keep-alive (t/every 5000 #(send-ping! conn (swap! msg-idx inc)))]
-      (s/consume (partial (:message-handler component) conn (swap! msg-idx inc)) conn)
-      ;; (s/on-closed conn #(prn 'slack-bot-connection-closed))
-      (assoc component :msg-idx msg-idx :conn conn :keep-alive keep-alive)))
+      ;; TODO use s/transform here to generate new sources (maybe)
+      (s/connect-via out-proxy #(add-id-and-jsonify conn (swap! msg-idx inc) %) conn)
+      (s/connect-via conn #(parse % in-proxy) in-proxy)
+      (s/on-closed conn #(timbre/infof "SlackConnection closed %s\n" ws-url))
+      (assoc component
+             :msg-idx msg-idx
+             :conn conn
+             :in-proxy in-proxy
+             :out-proxy out-proxy
+             :keep-alive keep-alive
+             :conversation-manager (component/start (conv/conversation-manager in-proxy out-proxy)))))
 
   (stop [component]
-    (println ";; Stopping SlackConnection" ws-url)
+    (timbre/infof "Stopping SlackConnection %s\n" ws-url)
     (s/close! (:conn component))
+    (s/close! (:in-proxy component))
+    (s/close! (:out-proxy component))
+    (component/stop (:conversation-manager component))
     ((:keep-alive component))
-    (dissoc component :conn :keep-alive :msg-idx)))
+    (dissoc component :conn :keep-alive :msg-idx :conversation-manager)))
 
-(defn slack-connection [token message-handler]
-  (map->SlackConnection {:ws-url (get-websocket-url token)
-                         :message-handler (or message-handler prn)}))
-
-(defn send-event! [^SlackConnection slack-conn msg]
-  ;; https://github.com/ztellman/manifold/issues/73
-  ;; (assert (:conn slack-conn) "Slack connection must be provided")
-  (->> (assoc msg :id (deref (:msg-idx slack-conn)))
-       (chesire/generate-string)
-       (s/put! (:conn slack-conn))))
+(defn slack-connection
+  "Retrieve a websocket connection url using the provided token and return a SlackConnection component"
+  [token]
+  (map->SlackConnection {:ws-url (get-websocket-url token)}))
 
 (defrecord SlackConnectionManager []
   component/Lifecycle
   (start [component]
-    (println ";; Starting SlackConnectionManager")
+    (timbre/info "Starting SlackConnectionManager")
     (assoc component :connections (atom {})))
   (stop [component]
-    (println ";; Stopping SlackConnectionManager")
+    (timbre/info "Stopping SlackConnectionManager")
     (doseq [c (vals @(:connections component))]
       (component/stop c))
     (dissoc component :connections)))
@@ -83,8 +106,8 @@
 (defn slack-connection-manager []
   (map->SlackConnectionManager {}))
 
-(defn initialize-connection! [^SlackConnectionManager conn-man bot-token msg-handler]
-  (let [c (component/start (slack-connection bot-token msg-handler))]
+(defn initialize-connection! [^SlackConnectionManager conn-man bot-token]
+  (let [c (component/start (slack-connection bot-token))]
     (swap! (:connections conn-man) assoc bot-token c)
     c))
 
@@ -94,43 +117,39 @@
 ;; REPL stuff =========================================================
 
 (comment
-  (def bot-user-id "U10AR0H50") ; retrieve from rethinkdb
-
-  ;; build out into generic slack-fmt w/ support for the various entities
-  (defn fmt-user [uid]
-    (str "<@" uid ">"))
-
-  (defn mentioned? [msg uid]
-    (string/includes? (:text msg) (fmt-user uid)))
-
-  (defn trace [x] (prn 'msg-debug x) x)
-
-  (defn reply! [conn idx msg]
-    (->> {:type "message" :id idx :channel (:channel msg)
-          :text (str (fmt-user (:user msg)) ": Hey there!")}
-         (chesire/generate-string)
-         (s/put! conn)))
-
-  (defn handle-msg [conn idx msg]
-    (let [msg (chesire/parse-string msg keyword)]
-      (when (:text msg)
-        (prn :mentioned (mentioned? msg bot-user-id)))
-      (when (and (:text msg) (mentioned? msg bot-user-id))
-        (reply! conn idx msg))
-      (prn msg)))
-
   (def bot
-    (map->SlackConnection {:ws-url (get-websocket-url token)
-                           :message-handler handle-msg}))
+    (map->SlackConnection {:ws-url (get-websocket-url (e/env :slack-bot-token))}))
 
   (alter-var-root #'bot component/start)
 
   (alter-var-root #'bot component/stop)
 
-  (send-ping! (:conn bot) 10)
+  ;; (def every-test (t/every 1000 #(send-ping! (:conn bot) 10)))
 
-  (def every-test (t/every 1000 #(send-ping! (:conn bot) 10)))
+  ;; (every-test)
 
-  (every-test)
+  ;; build out into generic slack-fmt w/ support for the various entities
+  ;; (defn fmt-user [uid]
+  ;;   (str "<@" uid ">"))
+
+  ;; (defn mentioned? [msg uid]
+  ;;   (string/includes? (:text msg) (fmt-user uid)))
+
+  ;; (defn trace [x] (prn 'msg-debug x) x)
+
+  ;; (defn reply! [conn idx msg]
+  ;;   (->> {:type "message" :id idx :channel (:channel msg)
+  ;;         :text (str (fmt-user (:user msg)) ": Hey there!")}
+  ;;        (chesire/generate-string)
+  ;;        (s/put! conn)))
+
+  ;; (defn handle-msg [conn idx msg]
+  ;;   (let [msg (chesire/parse-string msg keyword)]
+  ;;     (when (:text msg)
+  ;;       (prn :mentioned (mentioned? msg bot-user-id)))
+  ;;     (when (and (:text msg) (mentioned? msg bot-user-id))
+  ;;       (reply! conn idx msg))
+  ;;     (prn msg)))
+
 
   )
