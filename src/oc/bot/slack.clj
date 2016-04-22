b47cc60ea2feb090f937e2489164321149f20483
Martin Klepsch <martinklepsch@googlemail.com>
2016-04-22 14:24:55 +0200

    remove log line from slack ns

--- src/oc/bot/slack.clj
+++ src/oc/bot/slack.clj
@@ -50,7 +50,6 @@
 
 (defn parse [msg out]
   (let [m (chesire/parse-string msg keyword)]
-    (timbre/info m)
     (when-not (get m :ok ::ok)
       (timbre/warn "Error event from Slack" m))
     (s/put! out m)))
