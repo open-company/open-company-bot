2c5c2e32c7cc6e3c718a5b064195f7e1a13027f3
Martin Klepsch <martinklepsch@googlemail.com>
2016-04-28 15:31:04 +0200

    imprive api-client

--- src/oc/api_client.clj
+++ src/oc/api_client.clj
@@ -3,6 +3,7 @@
             [taoensso.timbre :as timbre]
             [cheshire.core :as cheshire]
             [manifold.deferred :as d]
+            [clojure.string :as string]
             [environ.core :as e]
             [byte-streams :as bs]))
 
@@ -19,10 +20,22 @@
 
 (def jwt "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyLWlkIjoic2xhY2s6VTA2U0JUWEpSIiwibmFtZSI6IlNlYW4gSm9obnNvbiIsInJlYWwtbmFtZSI6IlNlYW4gSm9obnNvbiIsImF2YXRhciI6Imh0dHBzOlwvXC9zZWN1cmUuZ3JhdmF0YXIuY29tXC9hdmF0YXJcL2Y1YjhmYzFhZmZhMjY2YzgwNzIwNjhmODExZjYzZTA0LmpwZz9zPTE5MiZkPWh0dHBzJTNBJTJGJTJGc2xhY2suZ2xvYmFsLnNzbC5mYXN0bHkubmV0JTJGN2ZhOSUyRmltZyUyRmF2YXRhcnMlMkZhdmFfMDAyMC0xOTIucG5nIiwiZW1haWwiOiJzZWFuQG9wZW5jb21wYW55LmNvbSIsIm93bmVyIjpmYWxzZSwiYWRtaW4iOnRydWUsIm9yZy1pZCI6InNsYWNrOlQwNlNCTUg2MCJ9.9Q8GNBojQ_xXT0lMtKve4fb5Pdh260oc2aUc-wP8dus")
 
+(def oc-json-types
+  ["application/vnd.open-company.company.v1+json"
+   "application/vnd.collection+vnd.open-company.company+json"
+   "application/json"])
+
+(defn json-type? [r]
+  (let [ct (or (get-in r [:headers "content-type"])
+               (get-in r [:headers "Content-Type"]))]
+    (some #(string/starts-with? ct %) oc-json-types)))
+
 (defn wrap-json [handler]
   (fn [request]
-    (let [res (handler (update request :body cheshire/generate-string))]
-      (d/chain res #(update % :body (comp cheshire/parse-string bs/to-string))))))
+    (let [->json  cheshire/generate-string
+          parse-bs (comp #(cheshire/parse-string % true) bs/to-string)
+          res      (handler (cond-> request (json-type? request) (update :body ->json)))]
+      (d/chain res #(cond-> % (json-type? %) (update :body parse-bs))))))
 
 (defn patch-company! [slug data]
   (timbre/info "Updating Company:" slug data)
@@ -33,11 +46,11 @@
                              "Accept-Charset" "utf-8"
                              "Content-Type" "application/vnd.open-company.company.v1+json"}
                    :body data})
-      (d/chain (fn [response] (timbre/info "Company Updated:" response)))
+      (d/chain (fn [response] (timbre/info "Company Updated:" (-> response :body :slug)) response) :body)
       (d/catch (fn [err] (timbre/error err)))))
 
 (comment 
-  @(patch-company! "abcr" {:description "Yada Yada"})
+  @(patch-company! "buffer" {:description "Yada Yada"})
 
   (-> (patch-company! "buffer" {:description "Yada Yada"})
       (d/chain :body prn)
@@ -45,7 +58,7 @@
 
 ;; (#'http/def-http-method 'patch)
 
-  (deref (http/get (str +api-host+ "/companies")))
+  (deref (http/get (str +api-host+ "/")))
 
   (deref (http/get (str +api-host+ "/companies/bago")))
 
