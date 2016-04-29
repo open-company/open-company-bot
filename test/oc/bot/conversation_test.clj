fd5c8c93db4f36c33776062f0972fdff3430f0ee
Martin Klepsch <martinklepsch@googlemail.com>
2016-04-29 13:31:46 +0200

    expand conversation_test.clj

--- test/oc/bot/conversation_test.clj
+++ test/oc/bot/conversation_test.clj
@@ -24,7 +24,35 @@
   (is (true? (conv/initialize? {:type :oc.bot/initialize :foo :bar :garbage 2 :receiver {:id 2}})))
   (is (false? (conv/initialize? {:type :oc.bot/initialize :foo :bar :garbage 2 :receiver {:channel 2}}))))
 
-(deftest msg-text->transition-test)
+(deftest msg-text->transition-test
+  (is (= [:yes] (conv/msg-text->transition "y" #{:yes})))
+  (is (nil? (conv/msg-text->transition "y" #{})))
+  (is (= [:yes] (conv/msg-text->transition "YES" #{:yes})))
+  (is (nil? (conv/msg-text->transition "YES" #{})))
+  (is (= [:no] (conv/msg-text->transition "no" #{:yes :no})))
+  (is (nil? (conv/msg-text->transition "no" #{})))
+
+  ;; Currency
+  (is (= [:currency ::conv/eur] (conv/msg-text->transition "eur" #{:currency :no})))
+  (is (nil? (conv/msg-text->transition "eur" #{:no})))
+  (is (= [:currency ::conv/eur] (conv/msg-text->transition "€" #{:currency :no})))
+  (is (nil? (conv/msg-text->transition "€" #{:no})))
+  (is (= [:currency ::conv/usd] (conv/msg-text->transition "$" #{:currency :no})))
+  (is (nil? (conv/msg-text->transition "$" #{:no})))
+
+  ;; Mixing Yes/No with wildcard string matches
+  (is (= [:no] (conv/msg-text->transition "no" #{:no :str})))
+  (is (= [:str "hello"] (conv/msg-text->transition "hello" #{:no :str})))
+  (is (= [:no] (conv/msg-text->transition "n" #{:no :yes :str})))
+  (is (= [:yes] (conv/msg-text->transition "yes" #{:no :yes :str}))))
+
+(deftest message-segment-id-test
+  (is (= [:init :yes] (conv/message-segment-id {:updated {} :stage :init} :yes)))
+  (is (= [:name :yes-after-update] (conv/message-segment-id {:updated {:name "hello"} :stage :name} :yes))))
+
+(deftest stage-confirmed?-test
+  (is (conv/stage-confirmed? {:value {:confirmed #{:name} :stage :name}}))
+  (is (not (conv/stage-confirmed? {:value {:confirmed #{:name} :stage :description}}))))
 
 (comment
   (test/run-tests)
