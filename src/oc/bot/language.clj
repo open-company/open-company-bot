b9e49c3b8f4f3b23805c35b439cc145af9ca490f
Martin Klepsch <martinklepsch@googlemail.com>
2016-06-16 23:25:56 +0200

    extract url and return when checking for downloadable image

--- src/oc/bot/language.clj
+++ src/oc/bot/language.clj
@@ -7,11 +7,12 @@
 (defn image? [s]
   (string/starts-with? s "image"))
 
-(defn valid-url? [url-str]
-  (let [validator (UrlValidator.)]
-    (when (.isValid validator url-str) url-str)))
+(defn extract-url [url-str]
+  (let [s (second (first (re-seq #"<(.*)>" url-str)))
+        validator (UrlValidator.)]
+    (when (.isValid validator s) s)))
 
-;; ================================
+;; ==================================================================
 
 (def yes? #{"y" "yes"})
 
@@ -22,13 +23,19 @@
 (def dollar? #{"$" "usd" "dollar"})
 
 (defn downloadable-image? [s]
-  (when (valid-url? s)
-    @(-> (http/head s)
+  (when-let [url (extract-url s)]
+    @(-> (http/head url)
          (d/chain #(when (and (= 200 (:status %))
                               (-> % :headers (get "content-type") image?))
-                     s))
+                     url))
          (d/catch (fn [_] false))))) ; aleph sets "content-type" https://github.com/ztellman/aleph/blob/1427d8142b1762244645425dc6bee685feb15a95/src/aleph/http/client_middleware.clj#L282-L289
 
 (comment
   (downloadable-image? "https://-R6qwnGcrHlY/AAAAAAAAAAI/AAAAAAAAAAA/SuoCUhq2DAM/w80-h80/photo.jpg")
-  (valid-url? "https://-R6qwnGcrHlY/AAAAAAAAAAI/AAAAAAAAAAA/SuoCUhq2DAM/w80-h80/photo.jpg"))
+  (valid-url "https://-R6qwnGcrHlY/AAAAAAAAAAI/AAAAAAAAAAA/SuoCUhq2DAM/w80-h80/photo.jpg")
+
+  (valid-url "<https://threaded.martinklepsch.org/fav/apple-icon-152x152.png>")
+
+  (re-seq #"<(.*)>" "<https://threaded.martinklepsch.org/fav/apple-icon-152x152.png>")
+
+  )
