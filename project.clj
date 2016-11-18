a74fd7999cc95e0747cfe3a5a0d5c45e15371de8
Sean Johnson <sean@snootymonkey.com>
2016-11-18 10:31:08 -0500

    Update dependencies.

--- project.clj
+++ project.clj
@@ -19,14 +19,14 @@
     [com.taoensso/timbre "4.8.0-alpha1"] ; Logging https://github.com/ptaoussanis/timbre
     [raven-clj "1.5.0"] ; Interface to Sentry error reporting https://github.com/sethtrain/raven-clj
     [com.stuartsierra/component "0.3.1"] ; Component Lifecycle https://github.com/stuartsierra/component
-    [funcool/cuerdas "2.0.0"] ; String manipulation library https://github.com/funcool/cuerdas
-    [medley "0.8.3"] ; Lightweight utility library https://github.com/weavejester/medley
+    [funcool/cuerdas "2.0.1"] ; String manipulation library https://github.com/funcool/cuerdas
+    [medley "0.8.4"] ; Lightweight utility library https://github.com/weavejester/medley
     [cheshire "5.6.3"] ; JSON encoding / decoding https://github.com/dakrone/cheshire
     [automat "0.2.0"] ; Finite state machines https://github.com/ztellman/automat
     [commons-validator "1.5.1"] ; provides the building blocks for validation https://commons.apache.org/proper/commons-validator/
     [stencil "0.5.0"] ; Clojure implementation of Mustache https://github.com/davidsantiago/stencil
     [clj-time "0.12.2"] ; Date and time lib https://github.com/clj-time/clj-time
-    [clj-http "3.3.0"] ; HTTP client https://github.com/dakrone/clj-http
+    [clj-http "3.4.1"] ; HTTP client https://github.com/dakrone/clj-http
     [aleph "0.4.2-alpha8"] ; Asynch comm. for clojure (http-client) https://github.com/ztellman/aleph don't upgrade to 0.4.2-alpha
     [manifold "0.1.6-alpha3"] ; Async programming tools https://github.com/ztellman/manifold
     [open-company/lib "0.0.6-04c024d"] ; Library for OC projects https://github.com/open-company/open-company-lib
@@ -63,14 +63,14 @@
         :log-level "trace"
       }
       :plugins [
-        [lein-bikeshed "0.4.0"] ; Check for code smells https://github.com/dakrone/lein-bikeshed
+        [lein-bikeshed "0.4.1"] ; Check for code smells https://github.com/dakrone/lein-bikeshed
         [lein-checkall "0.1.1"] ; Runs bikeshed, kibit and eastwood https://github.com/itang/lein-checkall
         [lein-pprint "1.1.2"] ; pretty-print the lein project map https://github.com/technomancy/leiningen/tree/master/lein-pprint
         [lein-ancient "0.6.10"] ; Check for outdated dependencies https://github.com/xsc/lein-ancient
         [lein-spell "0.1.0"] ; Catch spelling mistakes in docs and docstrings https://github.com/cldwalker/lein-spell
         [lein-deps-tree "0.1.2"] ; Print a tree of project dependencies https://github.com/the-kenny/lein-deps-tree
         [venantius/yagni "0.1.4"] ; Dead code finder https://github.com/venantius/yagni
-        [com.jakemccrary/lein-test-refresh "0.17.0"] ; Autotest https://github.com/jakemcc/lein-test-refresh
+        [com.jakemccrary/lein-test-refresh "0.18.0"] ; Autotest https://github.com/jakemcc/lein-test-refresh
       ]  
     }]
 
