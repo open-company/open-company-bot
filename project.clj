ee56090d5ef2e3c736123eb66c479ed6bcb86a8a
Sean Johnson <sean@snootymonkey.com>
2017-01-12 09:43:43 -0500

    Update dependencies.

--- project.clj
+++ project.clj
@@ -16,20 +16,20 @@
     [org.clojure/clojure "1.9.0-alpha14"] ; Lisp on the JVM http://clojure.org/documentation
     [org.clojure/core.async "0.2.395"] ; Async programming and communication https://github.com/clojure/core.async
     [environ "1.1.0"] ; Environment settings from different sources https://github.com/weavejester/environ
-    [com.taoensso/timbre "4.8.0-alpha1"] ; Logging https://github.com/ptaoussanis/timbre
+    [com.taoensso/timbre "4.8.0"] ; Logging https://github.com/ptaoussanis/timbre
     [raven-clj "1.5.0"] ; Interface to Sentry error reporting https://github.com/sethtrain/raven-clj
-    [com.stuartsierra/component "0.3.1"] ; Component Lifecycle https://github.com/stuartsierra/component
-    [funcool/cuerdas "2.0.1"] ; String manipulation library https://github.com/funcool/cuerdas
+    [com.stuartsierra/component "0.3.2"] ; Component Lifecycle https://github.com/stuartsierra/component
+    [funcool/cuerdas "2.0.2"] ; String manipulation library https://github.com/funcool/cuerdas
     [medley "0.8.4"] ; Lightweight utility library https://github.com/weavejester/medley
     [cheshire "5.6.3"] ; JSON encoding / decoding https://github.com/dakrone/cheshire
     [automat "0.2.0"] ; Finite state machines https://github.com/ztellman/automat
     [commons-validator "1.5.1"] ; provides the building blocks for validation https://commons.apache.org/proper/commons-validator/
     [stencil "0.5.0"] ; Clojure implementation of Mustache https://github.com/davidsantiago/stencil
-    [clj-time "0.12.2"] ; Date and time lib https://github.com/clj-time/clj-time
+    [clj-time "0.13.0"] ; Date and time lib https://github.com/clj-time/clj-time
     [clj-http "3.4.1"] ; HTTP client https://github.com/dakrone/clj-http
-    [aleph "0.4.2-alpha8"] ; Asynch comm. for clojure (http-client) https://github.com/ztellman/aleph don't upgrade to 0.4.2-alpha
-    [manifold "0.1.6-alpha3"] ; Async programming tools https://github.com/ztellman/manifold
-    [open-company/lib "0.0.6-04c024d"] ; Library for OC projects https://github.com/open-company/open-company-lib
+    [aleph "0.4.2-alpha12"] ; Asynch comm. for clojure (http-client) https://github.com/ztellman/aleph don't upgrade to 0.4.2-alpha
+    [manifold "0.1.6-alpha4"] ; Async programming tools https://github.com/ztellman/manifold
+    [open-company/lib "0.0.11-deee910"] ; Library for OC projects https://github.com/open-company/open-company-lib
   ]
 
   ;; All profile plugins
@@ -48,7 +48,7 @@
       ]
       :plugins [
         [jonase/eastwood "0.2.3"] ; Linter https://github.com/jonase/eastwood
-        [lein-kibit "0.1.2"] ; Static code search for non-idiomatic code https://github.com/jonase/kibit
+        [lein-kibit "0.1.3"] ; Static code search for non-idiomatic code https://github.com/jonase/kibit
       ]
     }
 
@@ -70,7 +70,7 @@
         [lein-spell "0.1.0"] ; Catch spelling mistakes in docs and docstrings https://github.com/cldwalker/lein-spell
         [lein-deps-tree "0.1.2"] ; Print a tree of project dependencies https://github.com/the-kenny/lein-deps-tree
         [venantius/yagni "0.1.4"] ; Dead code finder https://github.com/venantius/yagni
-        [com.jakemccrary/lein-test-refresh "0.18.0"] ; Autotest https://github.com/jakemcc/lein-test-refresh
+        [com.jakemccrary/lein-test-refresh "0.18.1"] ; Autotest https://github.com/jakemcc/lein-test-refresh
       ]  
     }]
 
