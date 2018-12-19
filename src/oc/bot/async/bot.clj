e1ee2f1fa081b9779cce9eecdaca2dd8994517ed
bago2k4 <iacopo@rc0.eu>
2018-12-19 13:43:36 +0100

    Read the team-id from the org to avoid issues with users that are on multiple teams.

--- src/oc/bot/async/bot.clj
+++ src/oc/bot/async/bot.clj
@@ -138,7 +138,7 @@
                       :last-name (:last-name msg)
                       :user-id (:user-id msg)
                       :avatar-url (:avatar-url msg)
-                      :team-id (first (:teams msg))}
+                      :team-id (:team-id org)} ;; Let's read the team-id from the org to avoid problems on multiple org users}
         id-token (jwt/generate-id-token token-claims c/passphrase)
         entry-url (s/join "/" [c/web-url
                                org-slug
