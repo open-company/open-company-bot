ceb6d350cbb2211cff6abe12dfdee06e5e5032ff
Nathan Zorn <nathan.zorn@gmail.com>
2019-01-28 16:17:12 -0500

    center dynamic text. change attachment color. set font color for dynamic text

--- src/oc/bot/image.clj
+++ src/oc/bot/image.clj
@@ -6,7 +6,7 @@
             [oc.bot.config :as c])
   (:import  [java.io File]
             [java.net URL]
-            [java.awt Font Color]
+            [java.awt Font FontMetrics Color]
             [java.awt.image BufferedImage]
             [javax.imageio ImageIO]))
 
@@ -15,27 +15,32 @@
   [org-slug]
   (str "/tmp/" org-slug ".png"))
 
+(defn s3-file-key
+  [org-slug text]
+  (clojure.string/lower-case
+    (str org-slug (clojure.string/replace text #"[\s\W]" "_") ".png")))
+
 (defn s3-url
-  [org-slug]
-  (str "https://" c/slack-digest-s3-bucket ".s3.amazonaws.com/" org-slug ".png"))
+  [org-slug text]
+  (str "https://" c/slack-digest-s3-bucket ".s3.amazonaws.com/" (s3-file-key org-slug text)))
 
 (defn exists-on-s3?
-  [org-slug]
+  [org-slug text]
   (try
-    (let [response (http/get (s3-url org-slug) {})
+    (let [response (http/get (s3-url org-slug text) {})
           status (:status response)]
       (= status 200))
     (catch Exception e
       false)))
 
 (defn write-to-s3
-  [org-slug]
+  [org-slug text]
   ;; put object with client side encryption
   (s3/put-object
     {:access-key c/aws-access-key-id
      :secret-key c/aws-secret-access-key}
     :bucket-name c/slack-digest-s3-bucket
-    :key (str org-slug ".png")
+    :key (s3-file-key org-slug text)
     :file (File. (tmp-file org-slug))
     :access-control-list {
      :grant-all [
@@ -55,25 +60,29 @@
         logo (ImageIO/read (org-logo logo))
         g (.getGraphics source)
         font-size 48 ;; 12px x 4
-        font (Font. "Helvitica" Font/BOLD font-size)
-        font-x (- (* (count banner-text) font-size) 900)]
-    ;; x and y are gathered from the background image.
+        font (Font. "Helvitica" Font/BOLD font-size)]
+    ;; x and y are hard coded from the background image.
     ;; scale logo to 212x212
     (.drawImage g logo 896 164 212 212 nil)
-    (.setColor g (Color/decode "34414F"))
+    (.setColor g (Color. 52, 65, 79)) ;;rgb of #34414F
     (.setFont g font)
-    (.drawString g banner-text font-x 625)
+    (let [fontx (quot ;; cal x position for font ((img width - font length) / 2)
+                  (-
+                    2000
+                    (.stringWidth
+                     (.getFontMetrics g)
+                     banner-text))
+                  2)]
+      (.drawString g banner-text fontx 625))
     (ImageIO/write (cast BufferedImage source) "png" (File. (tmp-file org-slug)))
-    (timbre/debug font-x)
-    (timbre/debug (Color/decode "34414F"))
     ;; upload image to s3
-    (write-to-s3 org-slug)))
+    (write-to-s3 org-slug banner-text)))
 
 (defn slack-banner-url
   [org-slug logo banner-text]
   ;; test if slack banner is on s3
-;;  (if (exists-on-s3? org-slug)
-;;    (s3-url org-slug)
-;;    (do
-  (generate-slack-banner org-slug logo banner-text)
-  (s3-url org-slug))
+  (if (exists-on-s3? org-slug banner-text)
+    (s3-url org-slug banner-text)
+    (do
+      (generate-slack-banner org-slug logo banner-text)
+      (s3-url org-slug banner-text))))
