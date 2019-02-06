a10920e3ece3daa1e994d831353c5a8c4316c502
Nathan Zorn <nathan.zorn@gmail.com>
2019-02-06 09:11:11 -0500

    copy change

--- src/oc/bot/digest.clj
+++ src/oc/bot/digest.clj
@@ -35,7 +35,7 @@
       :else
       clean-headline)))
 
-(def seen-text ":eyes:  You've seen this.")
+(def seen-text "✓ Seen")
 
 (defn- post-as-attachment [daily board-name {:keys [publisher url headline published-at comment-count comment-authors must-see video-id body uuid]} msg]
   (let [seen-data (get-seen-data msg uuid)
