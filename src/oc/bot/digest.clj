9b842543b92d6e973201046493dae3f827941c06
Nathan Zorn <nathan.zorn@gmail.com>
2019-02-18 07:45:34 -0500

    change display of 'others' in attribution.

--- src/oc/bot/digest.clj
+++ src/oc/bot/digest.clj
@@ -22,6 +22,45 @@
       :else
       clean-headline)))
 
+(defn attribution-text
+  "
+  Given the number of distinct authors to mention, the number of items, what to call the
+  item (needs to pluralize with just an 's'), and a sequence of authors of the items
+  to attribute (sequence needs to be distinct'able, and have a `:name` property per author),
+  return a text string that attributes the authors to the items.
+  E.g.
+  (attribution 3 7 'comment' [{:name 'Joe'} {:name 'Joe'} {:name 'Moe'} {:name 'Flo'} {:name 'Flo'} {:name 'Sue'}])
+  '7 comments by Joe, Moe, Flo and others'
+  "
+  [attribution-count item-count item-name authors]
+  (let [distinct-authors (distinct authors)
+        author-names (map :name (take attribution-count distinct-authors))
+        more-authors? (> (count distinct-authors) (count author-names))
+        multiple-authors? (> (count author-names) 1)
+        author-attribution (cond
+                              ;; more distinct authors than we are going to mention
+                              more-authors?
+                              (let [other-count (- (count distinct-authors)
+                                                   attribution-count)]
+                                (str (clojure.string/join ", " author-names)
+                                     " and "
+                                     other-count
+                                     " other"
+                                     (when (and (zero? other-count)
+                                                (> 1 other-count))
+                                       "s")))
+
+                              ;; more than 1 author so last mention needs an "and", not a comma
+                              multiple-authors?
+                              (str (clojure.string/join ", " (butlast author-names))
+                                                        " and "
+                                                        (last author-names))
+
+                              ;; just 1 author
+                              :else
+                              (first author-names))]
+    (str item-count " " item-name (when (> item-count 1) "s") " by " author-attribution)))
+
 (defn- attribution [comment-authors comment-count reaction-data receiver]
   (let [comments (text/attribution 3 comment-count "comment" comment-authors)
         reaction-authors (map #(hash-map :name %)
@@ -51,7 +90,7 @@
                                     (count reaction-data)
                                     "reaction"
                                     reaction-authors)
-        total-attribution (text/attribution 3
+        total-attribution (text/attribution 2
                                             (+ (count reaction-data)
                                                comment-count)
                                             "comments/reactions"
