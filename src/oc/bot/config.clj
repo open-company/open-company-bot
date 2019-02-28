3476b5fd4e83582b6e6abebfa26f0d24c5c04a5f
Sean Johnson <sean@snootymonkey.com>
2019-02-28 08:43:35 -0500

    Update Slack bot welcome copy.

--- src/oc/bot/config.clj
+++ src/oc/bot/config.clj
@@ -53,19 +53,18 @@
 ;; ----- Bot -----
 
 ;; https://api.slack.com/docs/message-formatting
-(defonce usage-bullets (str ">- I ensure comments from Carrot make it into Slack\n"
-                            ">- I also make sure posts shared from Carrot make it into Slack\n"
-                            ">- I unfurl links to Carrot that are sent in Slack messages\n" 
-                            ">- I let people know when they've been mentioned in Carrot\n" 
-                            ">- I notify people when they've been invited to a private board\n" 
-                            ">- I remind people when they need to update the team\n" 
-                            ">- And, I send a daily digest of new posts from the team"))
+(defonce usage-bullets (str ">- Provide a daily digest that keeps everyone focused on what matters most\n"
+                            ">- Add Carrot posts and comments to Slack\n"
+                            ">- Unfurl links to Carrot\n"
+                            ">- Make it easy to add new Carrot posts from Slack\n"
+                            ">- Notify people in Slack for Carrot mentions, comments, and invites\n" 
+                            ">- Remind people when it’s time to update so no one forgets"))
 (defonce usage-message (str "I'm the Carrot Bot, and it seems you have something to say to me. Well... I'm just a Carrot, I've got no ears!\n\n"
                             "Ha! 😜 I kid of course! But for the most part, I do like to stay deep in the soil, out of your way.\n\n"
                             "*Here's what I do:*\n"
                             usage-bullets))
-(defonce welcome-message (str "Hey there! Your Slack account has been successfully connected to Carrot.\n\n"
-                              "I'm the Carrot Bot; I work in the background to help out. Here's what I do:\n\n"
+(defonce welcome-message (str "Hey there! Your Slack account has been successfully connected to Carrot. The Carrot bot works in the background to keep Carrot and Slack in sync.\n\n"
+                              "*Here's what it does:*\n\n"
                               usage-bullets))
 
 (defonce slack-digest-s3-bucket (env :aws-s3-digest-banner-bucket))
