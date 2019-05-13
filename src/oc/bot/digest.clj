f0457b79fefedc389b4056d3a5a691cc6b0cb5b5
Calvin Sauer <calvin.j.sauer@gmail.com>
2019-05-13 19:09:18 -0400

    Update the way slack digest messages are chunked

--- src/oc/bot/digest.clj
+++ src/oc/bot/digest.clj
@@ -52,7 +52,7 @@
 (defn- markdown-post [url headline body]
   (str "<" url "|*" (slack-escaped-text headline) "*>\n" (slack-escaped-text body)))
 
-(defn- post-as-block [{:keys [publisher url headline published-at comment-count comment-authors board-name
+(defn- post-as-chunk [{:keys [publisher url headline published-at comment-count comment-authors board-name
                               interaction-attribution must-see video-id body uuid reactions]} msg]
   (let [seen-data (get-seen-data msg uuid)
         author-name (:name publisher)
@@ -110,36 +110,31 @@
       post-block
       separator-block])))
 
-(defn- sort-must-see-board-name [a b]
-  (let [must-see (compare (:must-see a) (:must-see b))]
-    (if (zero? must-see)
-      (let [board-name (compare (:board-name a) (:board-name b))]
-        (if (zero? board-name)
-          (compare (:published-at a) (:published-at b))
-          board-name))
-      must-see)))
-
-(defn- split-blocks
-  "Split message blocks into multiple message if over 16kb
-   https://api.slack.com/docs/rate-limits"
-  [blocks]
-  (let [default-split 5 ; 4 posts plus header
-        four-split (partition default-split default-split nil blocks)
-        four-bytes (.getBytes (json/generate-string (first four-split) "UTF-8"))
-        four-count (count four-bytes)
-        bytes (.getBytes (json/generate-string blocks) "UTF-8")
-        byte-count (count bytes)
-        byte-limit 6000] ; 16k is the limit but need to account for HTTP
-    (timbre/info "Slack limit?: " four-count byte-count byte-limit)
-    (if (> four-count byte-limit)
-      (let [parts-num (quot (count blocks)
-                            (inc (quot byte-count byte-limit)))
-            split-num (if (> default-split parts-num)
-                          default-split
-                          parts-num)
-            parts (partition split-num split-num nil blocks)]
-        {:intro (first parts) :rest (rest parts)})
-      {:intro (first four-split) :rest (rest four-split)})))
+(defn- build-slack-digest-messages
+  "
+  Given a banner-block, a footer-block, and a sequence of post-chunks, returns a sequence of messages
+  where each message is ready to pass to the slack/post-blocks function.
+  Here, chunk is defined as a sequence of blocks. While a post-chunk is considered one logical unit, it
+  is composed of multiple blocks in order to achieve stylized display in Slack.
+  Parition rules are as follows:
+  - If there's just 1 post:  all in 1 message
+  - If there's just 2 posts: banner and 1st post as 1 message, 2nd post and footer as 1 message
+  - if there's 3 posts:      banner and 1st post as 1 message, 2nd post as a message, 3rd post and footer as 1 message
+  - if there's 4+ posts:     banner and 1st post as 1 message, each middle post as a message, last post and footer as 1 message
+  With these partition rules the badge count that is displayed in Slack will match the post count
+  in Carrot (i.e. banner and footer blocks have no effect on badge count).
+  "
+  [banner-block post-chunks footer-block]
+  (let [num-post-chunks              (count post-chunks)
+        middle-post-chunks           (->> post-chunks (drop 1) (butlast))
+        header-post-chunk            (into [banner-block] (first post-chunks))
+        footer-post-chunk            (conj (vec (last post-chunks)) footer-block)]
+    (if (>= num-post-chunks 2)
+      (concat [header-post-chunk]
+              middle-post-chunks
+              [footer-post-chunk])
+      (concat [banner-block] (flatten post-chunks) [footer-block])
+      )))
 
 (defn- posts-with-board-name [board]
   (let [board-name (:name board)]
@@ -149,45 +144,38 @@
   {:pre [(string? token)
          (map? receiver)
          (map? msg)]}
-  (let [intro (str ":coffee: Good morning " (or org-name "Carrot"))
-        banner-block {:type "image"
-                      :image_url (image/slack-banner-url org-slug logo-url)
-                      :alt_text org-name
-                      :title {
-                        :type "plain_text"
-                        :emoji true
-                        :text org-name}}
+  (let [intro            (str ":coffee: Good morning " (or org-name "Carrot"))
+        banner-block     {:type      "image"
+                          :image_url (image/slack-banner-url org-slug logo-url)
+                          :alt_text  org-name
+                          :title     {
+                                      :type  "plain_text"
+                                      :emoji true
+                                      :text  org-name}}
         footer-selection (inc (rand-int 6)) ; 1 through 6
-        footer-block {:type "image"
-                      :image_url (image/slack-footer-url footer-selection)
-                      :alt_text "The end"
-                      :title {
-                       :type "plain_text"
-                       :text "The end"}}
+        footer-block     {:type      "image"
+                          :image_url (image/slack-footer-url footer-selection)
+                          :alt_text  "The end"
+                          :title     {
+                                      :type "plain_text"
+                                      :text "The end"}}
 
-        boards (map posts-with-board-name (:boards msg))
-        all-posts (mapcat :posts boards)
-        sorted-posts (sort sort-must-see-board-name all-posts)
-        must-see (filter :must-see sorted-posts)
-        non-must-see (filter (comp not :must-see) sorted-posts)
-        must-see-blocks (flatten (map #(post-as-block % msg) must-see))
-        regular-blocks (flatten (map #(post-as-block % msg) non-must-see))
-        ;; Remove the last divider
-        all-posts-blocks (butlast (concat must-see-blocks regular-blocks))
-        all-blocks (concat [banner-block]
-                            all-posts-blocks
-                            [footer-block])
-        split-blocks (split-blocks all-blocks)]
-    (timbre/debug "All blocks count:" (count all-blocks))
-    (timbre/debug "Split blocks:" split-blocks)
+        boards          (map posts-with-board-name (:boards msg))
+        all-posts       (mapcat :posts boards)
+        sorted-posts    (sort-by (juxt :must-see :board-name :published-at) all-posts)
+        must-see        (filter :must-see sorted-posts)
+        non-must-see    (filter (comp not :must-see) sorted-posts)
+        must-see-chunks (mapv #(post-as-chunk % msg) must-see)
+        regular-chunks  (mapv #(post-as-chunk % msg) non-must-see)
+        regular-chunks* (update regular-chunks (-> regular-chunks count dec) butlast) ;; remove the last divider
+        all-chunks      (concat must-see-chunks regular-chunks*)
+        digest-messages (build-slack-digest-messages banner-block
+                                                     all-chunks
+                                                     footer-block)
+        ]
+    (timbre/debug "Banner attachment:" banner-block)
+    (timbre/debug "Chunk count:" (count all-chunks))
     (timbre/debug "Footer attachment:" footer-block)
     (timbre/info "Sending digest to:" channel " with:" token)
-    (if (:intro split-blocks)
-      (do
-        (slack/post-blocks token
-                           channel
-                           (:intro split-blocks)
-                           intro)
-        (doseq [part (:rest split-blocks)]
-          (slack/post-blocks token channel part)))
-      (slack/post-blocks token channel (:rest split-blocks) intro))))
+    (doseq [msg digest-messages]
+      (slack/post-blocks token channel msg intro))))
