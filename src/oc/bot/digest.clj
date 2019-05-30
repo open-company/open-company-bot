429d10dd23cfd1e587a170fa023ffca5ad62b300
Sean Johnson <sean@snootymonkey.com>
2019-05-30 11:07:30 -0400

    Change abstract test to look for content.

--- src/oc/bot/digest.clj
+++ src/oc/bot/digest.clj
@@ -57,7 +57,7 @@
   (let [seen-data (get-seen-data msg uuid)
         author-name (:name publisher)
         clean-headline (post-headline headline)
-        reduced-body (or abstract (text/truncated-body body))
+        reduced-body (if (s/blank? abstract) (text/truncated-body body) abstract)
         accessory-image (html/first-body-thumbnail body)
         ;; if read/seen use seen attachment, else use button
         seen-this? (some #(= (:user-id msg) (:user-id %))
