0835e346bd73506a4c5a1d12d016f2d4835e0cb0
bago2k4 <iacopo@rc0.eu>
2020-09-13 15:25:58 +0200

    Fix sentry-appender call.

--- src/oc/bot/app.clj
+++ src/oc/bot/app.clj
@@ -53,8 +53,7 @@
   (if c/dsn
     (timbre/merge-config!
       {:level (keyword c/log-level)
-       :appenders {:sentry (sa/sentry-appender c/dsn {:environment c/sentry-env
-                                                      :release c/sentry-release})}})
+       :appenders {:sentry (sa/sentry-appender c/dsn)}})
     (timbre/merge-config! {:level (keyword c/log-level)}))
 
   ;; Echo config information
